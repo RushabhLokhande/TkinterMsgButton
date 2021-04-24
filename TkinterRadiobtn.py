58d64c7fb09e54e7aa5e48f1404620f7e9246045
RushabhLokhande <rushabhlokhande7171@gmail.com>
2021-04-24 02:06:29 -0700

    first commit

---
+++ TkinterRadiobtn.py
@@ -0,0 +1,7 @@
+from tkinter import *
+W = Tk()
+rb = Radiobutton(W, text='select 1')
+rb.pack()
+rb1 = Radiobutton(W, text='select 2')
+rb1.pack()
+W.mainloop()
---
+++ Tkintermsg.py
@@ -0,0 +1,7 @@
+from tkinter import *
+w = Tk()
+msg='Hello from Tkinter'
+i = Message(w,text = msg,bg='red')
+i.pack()
+
+w.mainloop()
